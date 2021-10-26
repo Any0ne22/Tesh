4c9aa51d19c5012d02980161e767c4640747fc88
Tom Gouville <tom.gouville@protonmail.com>
2021-10-26 15:38:28 +0200

    [ADD] Project structure

---
+++ src/prompt.h
@@ -0,0 +1,9 @@
+/** @file prompt.h
+ *  @brief Generate prompt
+ *
+ *  Generate a prompt containing username, hostname and working directory.
+ *
+ *  @author Tom Gouville (tom.gouville@telecomnancy.eu)
+ *  @author Amélie Ferstler (amelie.ferstler@telecomnancy.eu)
+ */
+#pragma once
