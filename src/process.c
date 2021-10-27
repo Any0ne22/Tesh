f62d553608ee3f9ca0c7168cb1e4ec35d24a1139
Tom Gouville <tom.gouville@protonmail.com>
2021-10-27 19:02:29 +0200

    [ADD] Create, launch and pipe processes

--- src/process.c
+++ src/process.c
@@ -1,1 +1,53 @@
 #include "process.h"
+
+process* new_process() {
+	process* p = calloc(1,sizeof(process));
+	pipe(p->fd_out);
+	p->status = -1;
+	return p;
+}
+
+process* piped_process(process* p1) {
+	process* p = calloc(1,sizeof(process));
+	p->fd_in[0] = p1->fd_out[0];
+	p->fd_in[1] = p1->fd_out[1];
+	pipe(p->fd_out);
+	p->status = -1;
+	return p;
+}
+
+int launch_process(process* p, char* args[], bool pipeOutput) {
+	if(args[0] == NULL) return -1;	//If there are no arguments
+	int pid = fork();
+	if(!pid) {
+		// Child process
+		if (pipeOutput) {
+			close(p->fd_out[0]);
+    		dup2(p->fd_out[1],STDOUT_FILENO);
+			close(p->fd_out[1]);
+		}
+
+		if(p->fd_in[0] != 0 || p->fd_in[1] != 0) {
+			// Pipe the standard input to the fd if it is initialized
+			close(p->fd_in[1]);
+    		dup2(p->fd_in[0],STDIN_FILENO);
+			close(p->fd_in[0]);
+		}
+
+		int errcode = execvp(args[0], args);
+		exit(errcode);
+	} else {
+		// Parent process
+		if(p->fd_in[0] != 0 || p->fd_in[1] != 0) {
+			// Close the descriptors if there was a pipe on the standard input
+			close(p->fd_in[0]);
+			close(p->fd_in[1]);
+		}
+		p->pid = pid;
+		return pid;
+	}
+}
+
+void free_process(process* p) {
+	free(p);
+}
--- src/process.h
+++ src/process.h
@@ -9,3 +9,62 @@
  *  @author Amélie Ferstler (amelie.ferstler@telecomnancy.eu)
  */
 #pragma once
+
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/wait.h>
+#include <stdbool.h> 
+#include <sys/types.h>
+
+/** @brief A struct for managing a process
+ * 
+ *  Allow to lauch a command in a process, get its status, pid, and to make pipes on
+ *  stdin or stdout.
+ */
+typedef struct _process {
+	int fd_in[2];	///< a file descriptor which can be piped to stdin
+	int fd_out[2];	///< a file descriptor which can be piped to stdout
+	int status;		///< the execution status of the process
+	pid_t pid;		///< the pid of the process
+} process;
+
+
+/** @brief New process struct
+ * 
+ *  Create a new process struct and initialize a pipe on process.fd_out
+ * 
+ *  @return a pointer to a process
+ */
+process* new_process();
+
+/** @brief New process struct with piped on another process
+ * 
+ *  Create a new process struct and pipe stdin on the stdout of
+ *  the process given in parameters.
+ * 
+ *  @param p1 the process piped
+ *  @return a pointer to a process
+ */
+process* piped_process(process* p1);
+
+/** @brief Lauch a command in a new process
+ * 
+ *  Launch a command (an array of arguments) in a new fork, manage pipes on
+ *  stdin and stdout and store the pid of the process.
+ *  If the command fails, the process exit with the error code of exec.
+ * 
+ *  @param p the process
+ *  @param args the command to launch (as an array of arguments, terminating with NULL)
+ *  @param pipeOutput pipe stdout on process.fd_out (true) or print stdout on terminal (false)
+ *  @return the pid of the process
+ */
+int launch_process(process* p, char* args[], bool pipeOutput);
+
+/** @brief Free a process
+ * 
+ *  Clear process from memory.
+ * 
+ *  @param p the process
+ *  @return void
+ */
+void free_process(process* p);
